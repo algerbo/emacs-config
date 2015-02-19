890594bbf3194ae387cb235ff4927bb8b99a7b99
algerbo <1302126565@qq.com>
2015-02-19 11:08:35 +0300

    Поправил формирование путей

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
-(require 'cask "/home/meph/.cask/cask.el")
+(require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 
 ;; load configuration files from here...
-(mapc 'load (directory-files "/home/meph/.emacs.d/customizations" t "^[0-9]+.*\.el$"))
+(mapc 'load (directory-files (concat (getenv "HOME") "/.emacs.d/customizations") t "^[0-9]+.*\.el$"))
 
 ;;; ### Следующие строки - это Emacs гадит... >=|;o)~ ###
 
